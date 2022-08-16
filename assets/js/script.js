a97eccb19e57adba8c099bb56f1a2ad1482d4773
Christian Smith <christian.46.smith@gmail.com>
2022-08-15 20:05:39 -0400

    added script tag to html and started basic js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var dateTimeEl = document.getElementById("dateTime");
+var localWeatherEl = document.getElementById("localWeather");
+var teamSelect = document.getElementById("teams");
+var forecastEl = document.getElementById("forecast");
+var matchupEl = document.getElementById("matchup");
+
+function searchTeam(event){
+    console.log()
+}
+
+
+
+teamSelect.addEventListener("change", function() {
+    console.log(this.value);
+});
