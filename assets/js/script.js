7fb2d1844d23c414b7d4910d12642a3e0d2f12ec
Christian Smith <christian.46.smith@gmail.com>
2022-08-18 19:05:39 -0400

    Show only upcoming games and clean up console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,6 @@ var matchupEl = document.getElementById("matchup");
 var espnURL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/";
 var weatherApiKey = "4024dd0ace3444c4f05da7654e63fece";
 
-// dateTimeEl.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-
-function showTime() {
-    dateTimeEl.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-}
-
 function init() {
     var team = JSON.parse(localStorage.getItem("saved-team"));
     if (team) {
@@ -24,7 +18,11 @@ function init() {
     displaySavedTeam();
 }
 
-function searchTeam(event){
+function showTime() {
+    dateTimeEl.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+
+function searchTeam(){
     console.log(this.value);
     var newTeamObject = {
         name: this.options[this.selectedIndex].text,
@@ -42,39 +40,38 @@ function displaySavedTeam(){
     
 }
 
-
-// console.log(data.events[0].competitions[0].venue.address["zipCode"]);
-// moment(data.events[0].competitions[0].date).format("MMMM Do h:mm:ss a")
 function getMatchups(abbreviation){
     fetch(espnURL + abbreviation + "/schedule")
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            // console.log(data);
             var events = data.events;
-            var eventCount;
-            if (events.length > 3) {
-                eventCount = 3;
-            } else {
-                eventCount = events.length;
-            }
-            for (let i = 0; i < eventCount; i++){
+            var eventCount = 0;
+            for (let i = 0; i < events.length; i++){
+                if (eventCount >= 2){
+                    break;
+                }
+
                 var zipCode = events[i].competitions[0].venue.address["zipCode"];
                 var header = events[i].name + " " + moment(data.events[i].competitions[0].date).format("MMMM Do h:mm a");
 
                 var daysUntil = moment(data.events[i].competitions[0].date).format("DDD") - moment().format("DDD");
+                if (daysUntil < 0){
+                    continue;
+                }
 
-                console.log(header);
                 if (zipCode){
-                    getLatLon(zipCode, header, i, daysUntil);
+                    getLatLon(zipCode, header, eventCount, daysUntil);
                 } else if (events[i].competitions[0].venue.fullName === "Paycor Stadium") {
-                    getLatLon(45202, header, i, daysUntil);
+                    getLatLon(45202, header, eventCount, daysUntil);
                 } else if (events[i].competitions[0].venue.fullName === "Acrisure Stadium"){
-                    getLatLon(15212, header, i, daysUntil);
+                    getLatLon(15212, header, eventCount, daysUntil);
                 } else {
                     console.log("No Zipcode Found");
                 }
+                eventCount++;
             }
         });
 }
@@ -106,12 +103,10 @@ function getWeather(lat, lon, header, index, daysUntil) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            displayMatchup(index, header, data.daily[daysUntil].weather[0].description + " " + data.daily[daysUntil].temp.day);
+            // console.log(data);
+            displayMatchup(index, header, data.daily[daysUntil].weather[0].description + " " + data.daily[daysUntil].temp.day + "°F");
         });
 }
-
-
 
 
 teamSelect.addEventListener("change", searchTeam);
